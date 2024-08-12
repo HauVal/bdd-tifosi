f7fe32a33a5273ca2629498207dd484ccb8f70b2
HauVal <valentin.hauspie@laposte.net>
2024-08-12 14:59:20 +0200

    add utilisateur

---
+++ utilisateurs-tifosi.sql
@@ -0,0 +1,5 @@
+# Privilèges pour `tifosi`@`%tifosi`
+
+GRANT ALL PRIVILEGES ON *.* TO `tifosi`@`%tifosi` IDENTIFIED BY PASSWORD '*AF78C3EF95879048E8FC640B2C0AAC1854E7E143' WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON `tifosi`.* TO `tifosi`@`%tifosi`;
